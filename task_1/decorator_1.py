dc838a540c39396bbc496c7047e3a6632b904047
Alex Bogatyrev <a.bogatyrev123@gmail.com>
2023-02-18 18:45:02 +0300

    initial

---
+++ task_1/decorator_1.py
@@ -0,0 +1,15 @@
+import datetime
+
+def logger(old_function):
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        log_data = f''' 
+        Дата и время вызова функции - {datetime.datetime.now()}\n 
+        Имя функции - {old_function.__name__}\n 
+        Аргументы - {args}, {kwargs}\n 
+        Результат функции - {result}\n'''
+        with open('task_1/main.log', 'a') as f:
+            f.writelines(log_data)
+        return result
+
+    return new_function
